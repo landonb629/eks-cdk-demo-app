e6d2dd7120428eb260a5753b00659192fb9e1a35
landon babay <landon.babay@caylent.com>
2023-12-15 01:03:54 -0500

    working on the authentication microservice

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ WIP. For the first iteration of this project, there won't be a functioning appli
 
 I'm going to attempt to integrate a background task that maybe can be run by a cron in kubernetes, I've never used them before so i'm interested to see what the use case for that could be 
 
+## API gateway 
+
+There will need to be an API gateway deployed that handles something as follows 
+
+client -> frontend -> api.test.com -> ingress controller -> api gateway 
+
+the API gateway should make sure that any requests that are coming to the application get routed based on path, ie: if you make a request to http://frontend.test.com/users that gets to the user microservice
--- services/auth/app.js
+++ services/auth/app.js
@@ -3,11 +3,38 @@ const axios = require('axios')
 const app = express()
 const cors = require('cors')
 
+/**
+ * auth -> 
+ * 1. get username and password 
+ * 2. hash the password (salt)
+ * 3. generate JWT for password 
+ */
 
 app.use(cors({origin: true}));
-app.get('/', async (req,res) => { 
-  console.log('hit auth service')
+app.use(express.json())
+app.get('/', async (req, res) => { 
   res.status(200).json({msg: 'OK'})
+})
+
+app.post('/login', async (req,res) => { 
+  try { 
+    const { username, password } = req.body
+    console.log(username);
+    console.log(password);
+    res.status(200).json({msg: 'OK'})
+  } catch(error) { 
+    console.log(error)
+  }
+  
+})
+
+app.post('/register', async (req, res) => { 
+  try { 
+
+  } catch(error) { 
+     console.log(error);
+     console.log(`ERROR registering user: ${error}`)
+  }
 })
 
 app.listen(3001, ()=> { 
